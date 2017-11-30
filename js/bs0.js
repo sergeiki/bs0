6a203312ce227f1c732a25cbd6d082f342aaa85b
sergeiki <sergei.ksmith@gmail.com>
2017-11-30 12:17:25 +0200

    bs0 v1

---
+++ js/bs0.js
@@ -0,0 +1,15 @@
+﻿$(".btn_card").on("click", function() {
+
+    var card = "#" + $(this).attr("id") + "_card";
+
+    if ($(this).hasClass("active")) {
+        $(card).hide();
+        $(this).text($(this).attr("name") + "OFF");
+    } else {
+        $(card).show();
+        $(this).text($(this).attr("name") + "ON");
+    }
+
+    $(this).button('toggle');
+
+})
