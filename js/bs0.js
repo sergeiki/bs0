14889eca1e8ae6e62da3c056b11c85d189e05ecf
sergeiki <sergei.ksmith@gmail.com>
2017-12-08 16:31:21 +0200

    Add files via upload

--- js/bs0.js
+++ js/bs0.js
@@ -11,5 +11,17 @@
     }
 
     $(this).button('toggle');
+})
 
+$(".card1_radio").on("click", function() {
+
+    var card_body = "#btn_card1_card";
+
+    $(card_body).removeClass("text-left text-center text-right");
+
+    switch ($(this).attr("id")) {
+        case "card1_radio1": $(card_body).toggleClass("text-left"); break;
+        case "card1_radio2": $(card_body).toggleClass("text-center"); break;
+        case "card1_radio3": $(card_body).toggleClass("text-right"); break;
+    }
 })
